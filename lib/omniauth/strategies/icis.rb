10a3c03d74093b62757407ddbbfd07070be178bf
Patrick Robertson <patricksrobertson@gmail.com>
2013-03-28 14:59:34 -0400

    Add state CSRF protection.

--- lib/omniauth/strategies/icis.rb
+++ lib/omniauth/strategies/icis.rb
@@ -4,9 +4,19 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Icis < OmniAuth::Strategies::OAuth2
-      option :name, "ICIS"
-
       option :client_options, { :site => 'https://icis-identity-example.herokuapp.com' }
+
+      def authorize_params
+        super.tap do |params|
+          if request.params['state']
+            params[:state] = request.params['state']
+
+            session['omniauth.state'] = params[:state]
+          end
+
+          params[:scope] ||= DEFAULT_SCOPE
+        end
+      end
     end
   end
 end
