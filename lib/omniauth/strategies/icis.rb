b5304ae5346578bd0254e0b6746e107cc55671d7
Patrick Robertson <patricksrobertson@gmail.com>
2013-04-05 09:01:16 -0400

    Add the info endpoint.

--- lib/omniauth/strategies/icis.rb
+++ lib/omniauth/strategies/icis.rb
@@ -6,6 +6,10 @@ module OmniAuth
     class Icis < OmniAuth::Strategies::OAuth2
       option :client_options, { :site => 'https://icis-identity-example.herokuapp.com' }
 
+      uid { raw_info["id"] }
+
+      info { { :email => raw_info["email"] } }
+
       def authorize_params
         super.tap do |params|
           if request.params['state']
@@ -14,6 +18,10 @@ module OmniAuth
             session['omniauth.state'] = params[:state]
           end
         end
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/v1/me.json').parsed
       end
     end
   end
